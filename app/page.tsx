96724a1cbfdea89325c007f052733c61d30dfefa
itskvsin <kevinsolankistudy@gmail.com>
2026-02-21 16:37:28 +0530

    added product info page 2

--- app/page.tsx
+++ app/page.tsx
@@ -5,19 +5,23 @@ import { useGSAP } from "@gsap/react";
 import gsap from "gsap";
 import { Lexend } from "next/font/google";
 import { useRef } from "react";
-
+// import Image from "next/image";
 
 const lexend = Lexend({
-  subsets: ['latin'],
-})
+  subsets: ["latin"],
+});
 
 export default function Home() {
-
   const fadeBg = useRef<HTMLDivElement>(null);
+  const amlaRef = useRef<HTMLDivElement>(null);
+  const leavesRef = useRef<HTMLDivElement>(null);
 
   useGSAP(() => {
     const bg = fadeBg.current;
-    if (!bg) return;
+    const amlaBg = amlaRef.current;
+    const leavesBg = leavesRef.current;
+
+    if (!bg || !amlaBg || !leavesBg) return;
 
     const tl = gsap.timeline({
       scrollTrigger: {
@@ -31,24 +35,129 @@ export default function Home() {
     tl.to(bg, {
       opacity: 0,
       duration: 1,
-    })
-  })
+    });
+
+    const amlas = gsap.utils.toArray<HTMLImageElement>(".amla");
+    amlas.forEach((amla, index) => {
+      gsap.fromTo(
+        amla,
+        { y: 200, duration: 1.4 },
+        {
+          y: -200,
+          opacity: 1,
+          ease: "none",
+          stagger: index * 1.8,
+          scrollTrigger: {
+            trigger: amlaBg,
+            start: "top bottom",
+            end: "bottom top",
+            scrub: true,
+          },
+        },
+      );
+    });
+
+    const leaves = gsap.utils.toArray<HTMLImageElement>(".leaf");
+    leaves.forEach((leaf, index) => {
+      gsap.fromTo(
+        leaf,
+        { y: 200, duration: 1 },
+        {
+          y: -200,
+          opacity: 1,
+          ease: "none",
+          stagger: index * 1.8,
+          scrollTrigger: {
+            trigger: leavesBg,
+            start: "top bottom",
+            end: "bottom top",
+            scrub: true,
+          },
+        },
+      );
+    });
+  });
 
   return (
-    <main className={`bg-white ${lexend.className}`}>
+    <main className={`bg-white ${lexend.className} overflow-hidden`}>
       <BottleScene />
 
-      <section ref={fadeBg} className="h-screen bg-[url(/images/amlaBg.png)] bg-center bg-no-repeat bg-cover text-white flex items-center justify-center">
-      <div className="h-screen w-screen bg-black/20 "></div>
-        <h1 className="text-pretty text-[160px] leading-tight font-extrabold w-1/6 absolute left-80 ">Nature's Answer To Hairfall</h1>
+      <section
+        ref={fadeBg}
+        className="h-screen bg-[url(/images/amlaBg.png)]  overflow-hidden bg-center bg-no-repeat bg-cover text-white flex items-center justify-center"
+      >
+        <div className="h-screen w-screen bg-black/20 "></div>
+        <h1 className="text-pretty text-[160px] leading-tight font-extrabold w-1/6 absolute left-80 ">
+          Nature's Answer To Hairfall
+        </h1>
       </section>
 
-    <section className="h-screen relative bg-linear-to-l  from-[#FFFEFE26] to-[#DFE2D2] text-[#4E482E] flex items-center justify-center">
-        <h1 className="text-pretty text-[160px] leading-tight font-normal w-1/6 absolute left-70 top-30 text-6xl">Rooted in <span className="font-extrabold">Amla</span></h1>
+      <section
+        ref={amlaRef}
+        className="h-screen relative bg-linear-to-l  from-[#FFFEFE26] to-[#DFE2D2] text-[#4E482E] flex items-center justify-center"
+      >
+        <h1 className="text-pretty text-[160px] leading-tight font-normal w-1/6 absolute left-70 top-30 text-6xl">
+          Rooted in <span className="font-extrabold">Amla</span>
+        </h1>
+        <img
+          src="/images/amla.png"
+          className="amla amla-1 absolute w-80 -top-40 left-10"
+        />
+        <img
+          src="/images/amla.png"
+          className="amla amla-2 absolute w-62 bottom-10 -left-10 rotate-300"
+        />
+        <img
+          src="/images/amla.png"
+          className="amla amla-3 absolute w-66 top-1/6 -right-20 rotate-320"
+        />
+        <img
+          src="/images/amla.png"
+          className="amla amla-4 absolute w-68 bottom-10 right-3/12 rotate-120"
+        />
+        {/* -- */}
+        <img
+          src="/images/amla.png"
+          className="amla amla-1 absolute w-60 top-3/10 left-3/14 rotate-18"
+        />
+        <img
+          src="/images/frontAmla.png"
+          className="amla amla-2 absolute w-62 z-99 -bottom-36 left-4/10 rotate-104"
+        />
+        <img
+          src="/images/frontAmla.png"
+          className="amla amla-3 absolute w-66 top-3/7 right-7/16"
+        />
+        <img
+          src="/images/frontAmla.png"
+          className="amla amla-4 absolute w-68 -bottom-40 right-1/10 rotate-140"
+        />
       </section>
 
-      <section className="h-screen bg-zinc-800 text-white flex items-center justify-center">
-        <h1 className="text-6xl">Section 3</h1>
+      <section
+        ref={leavesRef}
+        className="h-screen relative bg-linear-to-l  from-[#FFFEFE26] to-[#DFE2D2] text-[#4E482E] flex items-center justify-center"
+      >
+        {/* <Image 
+          src="/images/amlaBottle.png"
+          alt="Bottle"
+          height={100000000000}
+          width={100000000000}
+          className=" md:w-150"
+        /> */}
+        <h1 className="text-[#4E482E] absolute text-9xl w-2/3 text-right top-1/6 right-1/8 font-400">Calmed by <span className="font-extrabold">Ashwagandha</span></h1>
+        <img
+          src="/images/leaves/leaf1.png"
+          className="leaf leaf-3 absolute w-160 -bottom-5/14 left-46 rotate-20"
+        />
+        <img
+          src="/images/leaves/leaf3.png"
+          className="leaf leaf-2 absolute w-200 -top-170 -left-110 rotate-120"
+        />
+        <img
+          src="/images/leaves/leaf3.png"
+          className="leaf leaf-4 absolute w-180 -bottom-180 -right-1/12 z-99"
+        />
       </section>
 
       <section className="h-screen bg-zinc-700 text-white flex items-center justify-center">
--- components/BottleScene.tsx
+++ components/BottleScene.tsx
@@ -27,18 +27,20 @@ export default function BottleScene() {
     tl.fromTo(
       bottle,
       { y: 72,x: 50 ,opacity: 1, rotate: 82 },
-      { y: 0, opacity: 1,rotate: 76 ,duration: 1 },
+      { y: 0, opacity: 1,rotate: 45 ,duration: 1, scale: 1.5 },
     )
       .to(bottle, {
-        rotate: "-=136",
-        scale: 2.2,
+        rotate: "-=80",
+        scale: 2,
         yPercent: 80,
         xPercent: 30,
         duration: 1,
       })
       .to(bottle, {
-        x: 400,
-        rotate: "+=360",
+        rotate: "-=50",
+        scale: 1.8,
+        yPercent: 20,
+        xPercent: 40,
         duration: 1,
       })
       .to(bottle, {
@@ -52,11 +54,11 @@ export default function BottleScene() {
     <div className="fixed inset-0 flex items-center justify-center pointer-events-none z-50">
       <Image
         ref={bottleRef} 
-        src="/images/bottle.png"
+        src="/images/amlaBottle.png"
         alt="Bottle"
         height={100000000000}
         width={100000000000}
-        className="w-180"
+        className=" md:w-150"
       />
     </div>
   );
